d482469a4cf84330024e7a7ba9d5c26ff4875624
2020-DEV-168 <macbraine@gmail.com>
2020-10-19 14:04:33 +0200

    🔨❌ Add testGameContainingOnlyNumbers

--- Bowling/BowlingTests/GameTests.swift
+++ Bowling/BowlingTests/GameTests.swift
@@ -8,4 +8,14 @@
 import XCTest
 @testable import Bowling
 
-class GameTests: XCTestCase {}
+class GameTests: XCTestCase {
+
+    func testGameContainingOnlyNumbers() {
+        let scores = "17 23 81 23 44 51 61 71 25 61"
+        let expectedTotal = 70
+
+        let game = Game(scores: scores)
+
+        XCTAssertEqual(game.totalScore(), expectedTotal)
+    }
+}
