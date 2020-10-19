8a4fc192568309a427beb124b124732389ed9ad1
2020-DEV-168 <macbraine@gmail.com>
2020-10-19 14:06:47 +0200

    🔴 Add Game class

---
+++ Bowling/Bowling/Game.swift
@@ -0,0 +1,17 @@
+//
+//  Game.swift
+//  Bowling
+//
+//  Created by 2020-DEV-168 on 19/10/2020.
+//
+
+import Foundation
+
+class Game {
+
+    init(scores: String) {}
+
+    func totalScore() -> Int {
+        return 0
+    }
+}
